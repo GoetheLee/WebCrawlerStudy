87a1da5a5203a736efa53c640fa0972a3e203650
Goethe.Lee <goethe.lee@gmail.com>
2017-06-25 19:32:26 +0900

    add stock regression

--- Etrade/naver_linear_regression.py
+++ Etrade/naver_linear_regression.py
@@ -10,12 +10,13 @@ from __future__ import print_function
 import tensorflow as tf
 import numpy
 import matplotlib.pyplot as plt
+import re
 rng = numpy.random
 
 # data structure
 FORIGN_IDX = 1
 UPDOWN_IDX = 4
-CHK_DATE_SIZE = 2
+CHK_DATE_SIZE = 4
 
 # Parameters
 learning_rate = 0.01
@@ -24,18 +25,30 @@ display_step = 50
 
 # Training Data
 f = open("forigndata.txt", 'r')
+line = f.readline()
+line.replace(",", "")
+print(line)
+line.replace("%%", "")
+print(line)
+d1 = line.split()
+forign_buy = float(d1[FORIGN_IDX].replace(",","")) / 1000
+updown_ratio = float(d1[UPDOWN_IDX].replace("%",""))
+
+print("forign is ", forign_buy)
+print("up down is ", updown_ratio)
+
 while True:
     line = f.readline()
     if not line: break
     d1 = line.split()
-    forign_buy = d1[FORIGN_IDX]
-    updown_ratio = d1[UPDOWN_IDX]
-#    print(d1[FORIGN_IDX])
+    forign_buy = numpy.insert(forign_buy, 0, float(d1[FORIGN_IDX].replace(",","") )/1000)
+    updown_ratio = numpy.insert(updown_ratio, 0,  float(d1[UPDOWN_IDX].replace("%","")))
+
 
 '''train_X = numpy.asarray([[3.3,3.3], [4.4,4,4] ,[5.5,5.5], [6.71, 6],[6.93,6,9],
                          [4.168,4.1], [9.779,9.7], [6.182,6], [7.59,7.5], [2.167,2.1],
                          7.042,10.791,5.313,7.997,5.654,9.27,3.1])'''
-
+'''
 train_X_b = numpy.asarray([3.3,4.4,5.5,6.71,6.93,4.168,9.779,6.182,7.59,2.167,
                          7.042,10.791,5.313,7.997,5.654,9.27,3.1])
 train_X = numpy.array([[3.3, 3]])
@@ -45,15 +58,72 @@ for a in train_X_b:
 train_Y = numpy.asarray([1.7,2.76,2.09,3.19,1.694,1.573,3.366,2.596,2.53,1.221,
                          2.827,3.465,1.65,2.904,2.42,2.94,1.3])
 n_samples = train_Y.shape[0]
+'''
 
+n_samples = forign_buy.shape[0] - CHK_DATE_SIZE -1
+
+def get_arryed_data(input, start, size):
+    res = []
+    for a in range(0, CHK_DATE_SIZE):
+        res = numpy.append(res, [input[start + a]], axis=0)
+
+    res = [res]
+    for i in range(1, size - 1):
+        sub1 = []
+        for a in range(0, CHK_DATE_SIZE):
+            sub1 = numpy.append(sub1, [input[start + i + a]], axis=0)
+        #print(sub1)
+        res = numpy.append(res, numpy.array([sub1]), axis=0)
+
+    return res
+
+TRAINING_SIZE = int(n_samples * 0.75)
+TESTING_SIZE = n_samples - TRAINING_SIZE
+train_X = get_arryed_data(forign_buy, 0,  TRAINING_SIZE)
+#del forign_buy[0:TRAINING_SIZE]
+test_X = get_arryed_data(forign_buy, TRAINING_SIZE,  TESTING_SIZE)
+
+updown_ratio = numpy.delete(updown_ratio, 0, axis = 0)
+train_Y = updown_ratio[0:TRAINING_SIZE]
+#del updown_ratio[0:TRAINING_SIZE]
+test_Y = updown_ratio[TRAINING_SIZE:]
+'''
+print(train_X)
+print(train_Y)
+print(test_X)
+print(test_Y)
+exit()'''
+#train_X = numpy.array([[]])
+'''
+train_X = []
+for a in range(0, CHK_DATE_SIZE):
+    train_X = numpy.append(train_X, [forign_buy[a]], axis=0)
+
+train_X = [train_X]
+print(train_X)
+# print (train_X.shape)
+# train_X = [[ forign_buy[0], forign_buy[1], forign_buy[2], forign_buy[3] ]]
+for i in range( 1,n_samples-1):
+    sub1 = []
+    for a in range(0, CHK_DATE_SIZE):
+        sub1 = numpy.append(sub1,  [forign_buy[i+a]], axis = 0)
+    # sub1 = [[ forign_buy[i], forign_buy[i+1], forign_buy[i+2], forign_buy[i+3] ]]
+    print(sub1)
+    train_X = numpy.append(train_X, numpy.array([sub1]), axis = 0 )
+
+train_Y = numpy.delete(updown_ratio, 0, axis = 0)
+print(train_X)
+print(train_Y)
+exit()
+'''
 # tf Graph Input
 X = tf.placeholder("float64")
 Y = tf.placeholder("float64")
 
 # Set model weights
-W = tf.Variable(tf.zeros([ CHK_DATE_SIZE, 1]))
+#W = tf.Variable(tf.zeros([ CHK_DATE_SIZE, 1], dtype=tf.float64), name = "weight", dtype = tf.float64)
 
-W = tf.Variable([[rng.randn()],[rng.randn()]], name="weight",  dtype=tf.float64)
+W = tf.Variable([[rng.randn()],[rng.randn()], [rng.randn()], [rng.randn()]], name="weight",  dtype=tf.float64)
 #W = tf.Variable([[rng.randn()],[rng.randn()]], name="weight",  dtype=tf.float64)
 
 b = tf.Variable(rng.randn(), name="bias", dtype = tf.float64)
@@ -70,16 +140,17 @@ optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
 # Initializing the variables
 init = tf.global_variables_initializer()
 
+
 # Launch the graph
 with tf.Session() as sess:
     sess.run(init)
-
     # Fit all training data
     for epoch in range(training_epochs):
         avg_cost = 0
 
         for (x, y) in zip(train_X, train_Y):
             # {X: x, Y:y} cause crash
+            # print({X:[x], Y:y})
             _, c = sess.run([optimizer, cost], feed_dict={X: [x], Y: y})
             # sess.run(optimizer, feed_dict={X: [x], Y: y})
             avg_cost += c / n_samples
