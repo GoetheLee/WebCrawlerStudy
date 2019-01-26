fc09936982b7067b70e787727b179f16c4fc34ed
Hun Soo  Lee <hunsoolee@Hunui-MacBook-Pro.local>
2019-01-26 17:44:38 +0900

    Update stock price   update stock price example

--- Etrade/stock_price.py
+++ Etrade/stock_price.py
@@ -15,27 +15,26 @@ class stock(object):
         code = code_df.query("name=='{}'".format(item_name))['code'].to_string(index=False)
         return code
 
-    # 종목 이름을 입력하면 종목에 해당하는 코드를 불러와
-    # 네이버 금융(http://finance.naver.com)에 넣어줌
     def get_url(self, item_name):
         code_df = self.code_df
         code = code_df.query("name=='{}'".format(item_name))['code'].to_string(index=False)
         url = 'https://finance.naver.com/item/sise_day.nhn?code={code}'.format(code=code)
 
         return url
+    
+    def pre_process(self, idf):
+        pass
 
     def get_stock_data(self, item_name,  n_pages=20):
         code_df = self.code_df
-        url = get_url(item_name, code_df)
+        url = self.get_url(item_name)
 
         #Force certificate check and use certifi to handle the certificate. 
         https = urllib3.PoolManager( cert_reqs='CERT_REQUIRED',
             ca_certs=certifi.where(),)  
 
-        # 일자 데이터를 담을 df라는 DataFrame 정의
         df = pd.DataFrame()
 
-        # 1페이지에서 20페이지의 데이터만 가져오기
         for page in range(1, n_pages + 1):
             pg_url = '{url}&page={page}'.format(url=url, page=page)
             #print('url: ', pg_url)
@@ -44,4 +43,7 @@ class stock(object):
 
         # df.dropna()를 이용해 결측값 있는 행 제거
         df = df.dropna()
+        df['date'] = pd.to_datetime(df['날짜'])
+        df = df.set_index('date')
+        df = df.sort_index()
         return df
