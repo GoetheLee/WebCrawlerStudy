67108fc7af154d97d7d0cf08ec7d1c59e0706643
Hun Soo  Lee <hunsoolee@Hunui-MacBook-Pro.local>
2020-03-22 00:58:25 +0900

    add argparser

--- newscrawler/multi_hani_web_crawler.py
+++ newscrawler/multi_hani_web_crawler.py
@@ -1,13 +1,12 @@
-""" 한겨레 신문 특정 키워드를 포함하는, 특정 날짜 이전 기사 내용 크롤러(정확도순 검색)
-    python [모듈이름] [키워드] [가져올 페이지 숫자] [가져올 기사의 최근 날짜]
-    [결과 파일명.txt]
-    한페이지에 10개
+""" 
 """
 
 import sys
 from bs4 import BeautifulSoup
 import urllib.request
 from urllib.parse import quote
+import argparse
+import time
 
 TARGET_URL_BEFORE_KEWORD = 'http://search.hani.co.kr/Search?command=query&' \
                            'keyword='
@@ -16,41 +15,59 @@ TARGET_URL_BEFORE_UNTIL_DATE = '&media=news&sort=s&period=all&datefrom=' \
 TARGET_URL_REST = '&pageseq='
 
 
-def get_link_from_news_title(page_num, URL, output_file):
+def get_link_from_news_title(page_num, URL): #, output_file):
+    res = []
     for i in range(page_num):
         URL_with_page_num = URL + str(i)
         source_code_from_URL = urllib.request.urlopen(URL_with_page_num)
         soup = BeautifulSoup(source_code_from_URL, 'lxml',
-                             from_encoding='utf-8')
+                             from_encoding='utf-8')        
         for item in soup.select('dt > a'):
             article_URL = item['href']
-            get_text(article_URL, output_file)
+            res.append(article_URL)
 
+    return res
 
-def get_text(URL, output_file):
+def get_article_text(URL): #, output_file):
     source_code_from_url = urllib.request.urlopen(URL)
-    soup = BeautifulSoup(source_code_from_url, 'lxml', from_encoding='utf-8')
+    soup = BeautifulSoup(source_code_from_url, 'html.parser', from_encoding='utf-8')
     content_of_article = soup.select('div.text')
+    txt = ''
     for item in content_of_article:
-        string_item = str(item.find_all(text=True))
-        output_file.write(string_item)
+        txt += item.get_text(strip=True) # str(item.find_all(text=True))
+        
+    return {'title': soup.title.string, 
+            'datetime': [x.get_text() for x in soup.find('p', class_='date-time').contents],
+            'subtitle':soup.find("div", attrs={'class': 'subtitle'}).get_text(',', strip=True), # [x for x in soup.find("div", attrs={'class': 'subtitle'}).contents],
+            'contents': txt}
 
 
-def main(argv):
-    if len(sys.argv) != 5:
-        print("python [모듈이름] [키워드] [가져올 페이지 숫자] "
-              "[가져올 기사의 최근 날짜] [결과 파일명.txt]")
-        return
-    keyword = argv[1]
-    page_num = int(argv[2])
-    until_date = argv[3]
-    output_file_name = argv[4]
+def main(args):
+    start = time.perf_counter()
+    keyword = args.key
+    page_num = args.pagenum
+    until_date = args.datetime
+    #output_file_name = argv[4]
     target_URL = TARGET_URL_BEFORE_KEWORD + quote(keyword) \
                  + TARGET_URL_BEFORE_UNTIL_DATE + until_date + TARGET_URL_REST
-    output_file = open(output_file_name, 'w')
-    get_link_from_news_title(page_num, target_URL, output_file)
-    output_file.close()
-
+    urls = get_link_from_news_title(page_num, target_URL)
+    articles = []
+    for url in urls:
+        articles.append(get_article_text(url))
+    with open('test.txt', 'wt') as f:
+        for ar in articles:
+            f.write(str(ar))
+            f.write('\n\n')
+    print('done!! time: ', time.perf_counter() - start)
 
 if __name__ == '__main__':
-    main(sys.argv)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--key", type=str, default='',
+                    help="keyword for search")
+    parser.add_argument("--pagenum", type=int, default=2,
+                    help="crawl page number")
+    parser.add_argument("--datetime", type=str, # default=2,
+                    help="date for search")
+    args = parser.parse_args()
+    print(args)
+    main(args)
